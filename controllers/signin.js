3396e7bc07cd9d574b75fcaf703e28b73c7787cb
krylenger <krylenger@gmail.com>
2020-05-22 22:05:44 +0800

    fix

--- controllers/signin.js
+++ controllers/signin.js
@@ -18,7 +18,7 @@ const handleSignin = (req, res, db, bcrypt) => {
             res.status(400).json('wrong credentials');
         }
         })
-        .catch(err => console.log(err))
+        .catch(err => res.status(400).json('unable to signin'))
 }
 
 module.exports = {
